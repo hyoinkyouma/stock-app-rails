39a76abd58811d89fb3c5943dba2f9a97ef6617f
Roman <roman.cabalum@gmail.com>
2023-01-14 10:28:58 +0800

    minor bug fix to devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name])
   end
   
 end
